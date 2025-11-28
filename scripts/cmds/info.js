7897e934ef78997630da9b4e479d802e67990e4d
muskan10023 <crushqueen00001@gmail.com>
2025-11-29 01:41:03 +0600

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -55,7 +55,7 @@ module.exports = {
     await new Promise(resolve => setTimeout(resolve, 100));
     const ping = Date.now() - start;
 
-    const photoUrl = "https://i.imgur.com/muPx4cY.jpeg";
+    const photoUrl = "https://i.imgur.com/OGPBSxE.jpeg";
 
     const body = `
 ╭─ <𝐎𝐖𝐍𝐄𝐑  𝐈𝐍𝐅𝐎> ─╮
